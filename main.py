5d7700a1e593957f41889e41bce3b5369d780bf9
Lipenzo <192491525+Lipenzo@users.noreply.github.com>
2024-12-28 20:25:23 +0000

    main draw loop and window termination code

--- main.py
+++ main.py
@@ -2,9 +2,23 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    surf = pygame.Surface((10, 10))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        surf.fill("black")
+        pygame.display.flip()
+   
 
 if __name__ == "__main__":
     main()
