dcc8079244e3abfb9a3800476f765235ccf2af6c
Lipenzo <192491525+Lipenzo@users.noreply.github.com>
2024-12-28 20:31:53 +0000

    refactoring to mach example

--- main.py
+++ main.py
@@ -7,16 +7,13 @@ from constants import *
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
-    surf = pygame.Surface((10, 10))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        surf.fill("black")
+
+        screen.fill("black")
         pygame.display.flip()
    
 
